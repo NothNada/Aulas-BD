c548fc86ad2b6638d32be37308250b54873c3244
Rubens Gabriel <grubens476@gmail.com>
2025-04-01 14:33:20 -0300

    Adicionando inserts

--- comandos.sql
+++ comandos.sql
@@ -10,3 +10,15 @@ CREATE TABLE produtos (
     valor INT,
     descricao TEXT
 );
+
+INSERT INTO usuarios (nome, senha) VALUES ('João Silva', 'senha123');
+INSERT INTO usuarios (nome, senha) VALUES ('Maria Oliveira', 'senha456');
+INSERT INTO usuarios (nome, senha) VALUES ('Thomas Turbano', 'senha789');
+INSERT INTO usuarios (nome, senha) VALUES ('Deide Costa', 'senha101');
+INSERT INTO usuarios (nome, senha) VALUES ('Cuca Beludo', 'senha202');
+
+INSERT INTO produtos (imagem, valor, descricao) VALUES ('imagem1.jpg', 10, 'Naftalina');
+INSERT INTO produtos (imagem, valor, descricao) VALUES ('imagem2.jpg', 25, 'Balinha');
+INSERT INTO produtos (imagem, valor, descricao) VALUES ('imagem3.jpg', 10, 'Dipirona');
+INSERT INTO produtos (imagem, valor, descricao) VALUES ('imagem4.jpg', 10, 'Tadalafila');
+INSERT INTO produtos (imagem, valor, descricao) VALUES ('imagem5.jpg', 15, 'Analgesico');
